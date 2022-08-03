a4d123b961abd5b52b2e2bb33619fd42069815a9
aabhas_jindal <aabhas@cashfree.com>
2022-08-03 19:37:48 +0530

    [UPDATE] React and React Native version.

--- example/ios/CashfreePgApiExample/AppDelegate.m
+++ example/ios/CashfreePgApiExample/AppDelegate.m
@@ -6,7 +6,9 @@
  */
 
 #import "AppDelegate.h"
-
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -37,6 +39,10 @@ static void InitializeFlipper(UIApplication *application) {
     InitializeFlipper(application);
   #endif
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  #if RCT_DEV
+  [bridge moduleForClass:[RCTDevLoadingView class]];
+
+  #endif //-------------------
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"CashfreePgApiExample"
                                             initialProperties:nil];
