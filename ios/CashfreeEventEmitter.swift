391a99e52f5712525c5ec446be2b995c77ca9995
aabhas_jindal <aabhas@cashfree.com>
2022-08-03 19:15:03 +0530

    [Fix] Module CashfreePgApi.swift and CashfreeEventEmitter.swift requires main queue setup since it overrides init but doesn't implement requiresMainQueueSetup

--- ios/CashfreeEventEmitter.swift
+++ ios/CashfreeEventEmitter.swift
@@ -9,16 +9,20 @@ import Foundation
 
 @objc(CashfreeEventEmitter)
 open class CashfreeEventEmitter: RCTEventEmitter {
-    
+
     override init() {
         super.init()
         CashfreeEmitter.sharedInstance.registerEventEmitter(eventEmitter: self)
     }
-    
+
     /// Base overide for RCTEventEmitter.
     ///
     /// - Returns: all supported events
     @objc open override func supportedEvents() -> [String] {
         return CashfreeEmitter.sharedInstance.allEvents
+    }
+
+    @objc public override static func requiresMainQueueSetup() -> Bool {
+        return false
     }
 }
