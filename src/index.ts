110615504816e27be31d9613f23b44e0310ca4b2
aabhas_jindal <aabhas@cashfree.com>
2023-04-26 19:32:42 +0530

    update cashfree-pg-api-contract to 2.0.1-dev.0

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,20 @@ import type { CheckoutPayment, CFSession } from 'cashfree-pg-api-contract';
 
 const LINKING_ERROR =
   `The package 'react-native-cashfree-pg-api' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: '- You have run \'pod install\'\n', default: '' }) +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
 const CashfreePgApi = NativeModules.CashfreePgApi
   ? NativeModules.CashfreePgApi
   : new Proxy(
-    {},
-    {
-      get() {
-        throw new Error(LINKING_ERROR);
-      },
-    },
-  );
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 class CFPaymentGateway {
   private emitter: EventEmitter;
@@ -56,7 +56,7 @@ class CFPaymentGateway {
       };
       this.eventSubscription = this.emitter.addListener(
         'cfEvent',
-        eventFunction,
+        eventFunction
       );
       CashfreePgApi.setEventSubscriber();
     }
@@ -92,11 +92,11 @@ class CFPaymentGateway {
     };
     this.successSubscription = this.emitter.addListener(
       'cfSuccess',
-      successFunction,
+      successFunction
     );
     this.failureSubscription = this.emitter.addListener(
       'cfFailure',
-      failureFunction,
+      failureFunction
     );
     CashfreePgApi.setCallback();
   }
