721af1b9f1c3ab3f5fe0daf340584b6ffb6a1569
Kishan_maurya <kishan.maurya@cashfree.com>
2026-02-23 16:52:25 +0530

    android & iOS SDK updated

--- ios/CashfreePgApi.swift
+++ ios/CashfreePgApi.swift
@@ -6,7 +6,7 @@ import CashfreePG
 class CashfreePgApi: NSObject {
 
     var analyticsCallbackEnabled: Bool = false
-    private let versionNumber = "2.2.4"
+    private let versionNumber = "2.2.7"
 
     override init() {
         super.init()
